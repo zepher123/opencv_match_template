fd384839845b51ea7d9c9a08f0b54f0456e41ad6
utsav <utsavpaudel29@gmail.com>
2023-07-11 07:49:54 +0545

    draw rectangle around matched part

--- matching_template.py
+++ matching_template.py
@@ -11,9 +11,15 @@ w, h = img_temp.shape[::-1]
 res_match = cv.matchTemplate(gray_img, img_temp, cv.TM_CCORR_NORMED)
 print(res_match)
 
+
+
 threshold = 0.99
 pass_thres = np.where(res_match >= threshold)
 print(pass_thres)
+
+
+for temp_pt in zip(*pass_thres[::-1]):
+    cv.rectangle(img, temp_pt, (temp_pt[0] + w, temp_pt[1]+h), (255, 0, 0), 2)
 
 cv.imshow("Image", img)
 cv.waitKey()
