76c0c21d2ce755a8906aa22adfda76b19a88b0b5
utsav <utsavpaudel29@gmail.com>
2023-07-11 07:14:02 +0545

    mactched array print

--- matching_template.py
+++ matching_template.py
@@ -3,9 +3,13 @@ import numpy as np
 
 
 img = cv.imread("ronaldo.jpg")
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_temp = cv.imread("ball.jpg", 0)
 
+w, h = img_temp.shape[::-1]
 
-
+res_match = cv.matchTemplate(gray_img, img_temp, cv.TM_CCORR_NORMED)
+print(res_match)
 
 cv.imshow("Image", img)
 cv.waitKey()
