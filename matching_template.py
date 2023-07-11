4b64e153afaeaa7f50c62e830b43b37c9f35a1bc
utsav <utsavpaudel29@gmail.com>
2023-07-11 07:34:54 +0545

    0.99 matched threshold array print

--- matching_template.py
+++ matching_template.py
@@ -11,6 +11,10 @@ w, h = img_temp.shape[::-1]
 res_match = cv.matchTemplate(gray_img, img_temp, cv.TM_CCORR_NORMED)
 print(res_match)
 
+threshold = 0.99
+pass_thres = np.where(res_match >= threshold)
+print(pass_thres)
+
 cv.imshow("Image", img)
 cv.waitKey()
 cv.destroyAllWindows()
